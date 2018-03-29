56c661e67eb53705e10a5b2041fcf11957b69fea
Pavel Zastavnitskiy <pavel.zastavnitskiy@booking.com>
2018-03-29 17:42:36 +0200

    Sparcle discussion

--- src/fetchers/copy-fetcher.js
+++ src/fetchers/copy-fetcher.js
@@ -7,6 +7,27 @@ import * as fs from '../util/fs.js';
 export default class CopyFetcher extends BaseFetcher {
   async _fetch(): Promise<FetchedOverride> {
     await fs.copy(this.reference, this.dest, this.reporter);
+  
+    /* 
+    Question one: once we have a copy of the file tree, we have 2 options,
+
+      Option one: 
+        read files field from package.json
+        delete all the files that do not match regexp
+        
+      Option two:
+        run pack command for this directory
+
+      Each has downsides – with first one, we will be missing out on other 
+      task that publish may perform.
+
+      For the second one, we may have issues, with repos that are not intended for publising. Maybe.
+      
+    Question two?
+      Should we also do this for other fetchers? Which?
+
+    */
+    
     return {
       hash: this.hash || '',
       resolved: null,
